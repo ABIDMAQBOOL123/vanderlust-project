e14679931d704214c373ed52ac3c0a9bf4b1f246
abid maqbool <abidmaqbool26@gmail.com>
2025-01-08 18:36:56 +0530

    refactored code

--- app.js
+++ app.js
@@ -1,87 +1,79 @@
-if(process.env.NODE_ENV != "production"){
+if (process.env.NODE_ENV != "production") {
     require('dotenv').config();
 }
-
- 
 
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
 const path = require("path");
-const methodOverride= require("method-override");
+const methodOverride = require("method-override");
 
 const ejsMate = require("ejs-mate");
 
-const ExpressError = require("./utils/ExpressErrors.js")
-const session = require("express-session")
+const ExpressError = require("./utils/ExpressErrors.js");
+const session = require("express-session");
 const MongoStore = require('connect-mongo');
-const flash = require("connect-flash")
-const passport = require("passport")
+const flash = require("connect-flash");
+const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User =  require("./models/user.js");
+const User = require("./models/user.js");
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
+const dbUrl = process.env.ATLASDB_URL;
 
 
+// console.log("Database URL:", dbUrl);
 
-const listingRouter = require("./routes/listing.js")
-const reviewRouter = require("./routes/review.js")
-const userRouter = require("./routes/user.js")
-
-
-
-
-
-const dbUrl = process.env.ATLASDB_URL
-// const db = 'mongodb://localhost:27017';
-
-
-
-main().then(()=>{
-    console.log("connected to DB")
-})
-.catch((err) =>{
-    console.log(err);
-})
-
-async function main(){
-    await mongoose.connect(db)
+if (!dbUrl) {
+    console.error("ERROR: Database URL is undefined. Check your .env file or environment variables.");
+    process.exit(1); // Exit the process as it's a critical error
 }
 
+main()
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch((err) => {
+        console.error("ERROR: Unable to connect to the database:", err.message);
+    });
+
+async function main() {
+    await mongoose.connect(dbUrl);
+}
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"))
-app.use(express.urlencoded({extended:true}));
-app.use(methodOverride("_method"))
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
-
+app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
-    mongoUrl:  dbUrl,
-    crypto:{
-     secret: process.env.SECRET,
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
     },
-    touchAfter:24 *3600,
- });
+    touchAfter: 24 * 3600,
+});
 
- store.on("error", () =>{
-    console.log("ERROR in MONGO SESSION STORE", err)
- });
+store.on("error", (err) => {
+    console.error("ERROR in MONGO SESSION STORE:", err);
+});
 
 const sessionOptions = {
     store,
-    secret:process.env.SECRET,
-    resave:false,
-    saveUninitialised: true,
-    cookie:{
-        expires:Date.now() +7 *24 *60 * 60* 1000,
-        httpOnly: true
-    }
-}
-
-
-
-
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+};
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -93,61 +85,26 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser =req.user
+    res.locals.currUser = req.user;
     next();
 });
 
-
-app.get("/demoUser", async(req, res)=>{
-    let fakeUser = new User({
-        email:"student@gmail.com",
-        username: "delta-student"
-    });
-   let registeredUser = await User.register(fakeUser, "helloworld");
-   res.send(registeredUser);
-})
-
-
-
-
-
-
-
-
-
-
 app.use("/listings", listingRouter);
-
-
-app.use("/listings/:id/reviews", reviewRouter)
+app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.all("*",(req, res, next) =>{
-    next(new ExpressError(404, "page not found"))
-})
-
-app.use((err, req, res, next) =>{
-    let {statusCode=500, message="something went wrong"}=err;
-    res.status(statusCode).render("error.ejs", {message})
-    //  res.status(statusCode).send(message)
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page not found"));
 });
 
-app.listen(8080, ()=>{
-    console.log("server is listening to port 8080")
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = "Something went wrong" } = err;
+    res.status(statusCode).render("error.ejs", { message });
+});
+
+app.listen(8080, () => {
+    console.log("Server is listening on port 8080");
 });
